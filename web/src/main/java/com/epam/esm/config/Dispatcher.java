c1e44ce60e09ea9dadf8da9b099591b827a4a347
Kyrylo Pavlenko <kyrylo.pavlenko.w@gmail.com>
2022-12-22 14:11:04 +0200

    Initial commit

---
+++ web/src/main/java/com/epam/esm/config/Dispatcher.java
@@ -0,0 +1,29 @@
+package com.epam.esm.config;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class Dispatcher extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected DispatcherServlet createDispatcherServlet(WebApplicationContext servletAppContext) {
+        DispatcherServlet ds = new DispatcherServlet(servletAppContext);
+        ds.setThrowExceptionIfNoHandlerFound(true);
+        return ds;
+    }
+}
---
+++ web/src/main/java/com/epam/esm/controllers/GreetingsController.java
@@ -0,0 +1,21 @@
+package com.epam.esm.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class GreetingsController {
+
+    @GetMapping
+    public List<String> greetings(){
+        List<String> list = new ArrayList<>();
+        list.add("Hello");
+        list.add("Spring");
+        return list;
+    }
+}
