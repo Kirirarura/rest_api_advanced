02a35a9a555152759a03f9fbe4a4c7b10e35b9fe
Kyrylo Pavlenko <kyrylo.pavlenko.w@gmail.com>
2022-12-27 16:06:53 +0200

    Added DaoExceptions

---
+++ model/src/main/java/com/epam/esm/exceptions/ExceptionDaoMessages.java
@@ -0,0 +1,13 @@
+package com.epam.esm.exceptions;
+
+public class ExceptionDaoMessages {
+
+    private ExceptionDaoMessages() {
+    }
+
+    public static final String NO_ENTITY = "404000";
+    public static final String NO_ENTITY_WITH_ID = "404001";
+    public static final String NO_ENTITY_WITH_PARAMETERS = "404002";
+    public static final String NO_ENTITY_WITH_NAME = "404003";
+    public static final String SAVING_ERROR = "404004";
+}
