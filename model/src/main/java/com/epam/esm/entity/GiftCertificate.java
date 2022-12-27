66a3c17e030005dc0a97bb5bf1bac8ed0e8ac809
Kyrylo Pavlenko <kyrylo.pavlenko.w@gmail.com>
2022-12-27 16:08:01 +0200

    Added Entities (GiftCertificate, Tag)

---
+++ model/src/main/java/com/epam/esm/entity/GiftCertificate.java
@@ -0,0 +1,121 @@
+package com.epam.esm.entity;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class GiftCertificate {
+
+    private long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private int duration;
+    private String createDate;
+    private String lastUpdateDate;
+
+    @JsonCreator
+    public GiftCertificate() {
+    }
+
+    public GiftCertificate(long id, String name, String description, BigDecimal price,
+                           int duration, String createDate, String lastUpdateDate) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.duration = duration;
+        this.createDate = createDate;
+        this.lastUpdateDate = lastUpdateDate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    public void setLastUpdateDate(String lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GiftCertificate that = (GiftCertificate) o;
+        return getId() == that.getId() &&
+                getDuration() == that.getDuration() &&
+                getName().equals(that.getName()) &&
+                getDescription().equals(that.getDescription()) &&
+                getPrice().equals(that.getPrice()) &&
+                getCreateDate().equals(that.getCreateDate()) &&
+                getLastUpdateDate().equals(that.getLastUpdateDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getDescription(), getPrice(), getDuration(),
+                getCreateDate(), getLastUpdateDate());
+    }
+
+    @Override
+    public String toString() {
+        return "GiftCertificate{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", duration=" + duration +
+                ", createDate=" + createDate +
+                ", lastUpdateDate=" + lastUpdateDate +
+                '}';
+    }
+}
